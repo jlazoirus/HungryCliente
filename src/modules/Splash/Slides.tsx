3f22bc88e90c9ae4830fd403a08d72a1832575d0
isaac luis mori <luismorith@gmail.com>
2018-05-27 22:59:50 -0500

    adding typings to the new react navigation version 2.0

--- src/modules/Splash/Slides.tsx
+++ src/modules/Splash/Slides.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ScrollView, View, Text, Dimensions, Button } from "react-native";
+import { ScrollView, View, Text, Dimensions, Button, Image } from "react-native";
 import styled from 'styled-components';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
@@ -7,7 +7,8 @@ const Slide = styled(View)`
     flex: 1;
     justify-content: center;
     align-items: center;
-    width: ${SCREEN_WIDTH}
+    width: ${SCREEN_WIDTH};
+    background-color: #aa072a;
 `;
 
 const SlideText = styled(Text)`
@@ -29,14 +30,17 @@ class Slides extends React.Component<Props, State> {
     
 
   render() {
-      const lastScreenIndex = this.props.data.length - 1;
       return (
         <ScrollView horizontal pagingEnabled style={{flex: 1}}>
             { this.props.data && this.props.data.map((slide, index) => (
                     <Slide key={slide.text}>
-                        <SlideText>{slide.text}</SlideText>
-                        <Button 
-                            title={index === lastScreenIndex ? 'Get started' : 'skip' } 
+                        <Image 
+                            style={{width: 240, height: 150}} 
+                            source={require('../../../assets/images/hungrylogo.png')} 
+                        /> 
+                        <Button
+                            color="white"
+                            title={'Comencemos!'} 
                             onPress={this.skipSplashScreen} /> 
                     </Slide>
                 )
