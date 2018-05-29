195b2520f398bd32aaba4c98c00b918230e51d97
Jonathan Lazo <j.lazo.irus@live.com>
2018-05-29 00:44:05 -0500

    add historial

---
+++ src/modules/Historial/HistorialTeaser.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { Text, View, Image } from "react-native";
+import { H4 } from "nachos-ui";
+import { Icon } from "native-base";
+import styled from "styled-components";
+
+const S = {
+    Card: styled(View)`
+        flex-direction: row;
+        padding: 10px;
+        border-bottom-color: #aa072a;
+        border-bottom-width: 1px;
+    `,
+    Image: styled(View)`
+        flex: 2
+    `,
+    Content: styled(View)`
+        flex: 5;
+        padding-left: 10px;
+    `,
+    ViewMore: styled(View)`
+        flex: 1;
+        justify-content: center;
+    `,
+};
+
+type Props = {
+  key: any
+}
+
+export default class HistorialTeaser extends React.Component<Props, any> {
+
+
+  render() {
+    return (
+      <S.Card>
+        <S.Image>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: "https://upx.cz/56w" }}
+          />
+        </S.Image>
+        <S.Content>
+          <H4>NOMBRE DEL LOCAL</H4>
+          <Text>Fecha</Text>
+          <Text>Importe</Text>
+          <Text>Descripcion del pedido</Text>
+        </S.Content>
+        <S.ViewMore>
+          <Icon name="help-with-circle" />
+        </S.ViewMore>
+      </S.Card>
+    );
+  }
+}
