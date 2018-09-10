7eb1aa87b6342a71d797e097399411f78301da4f
mvergaray <marco.vergaray@hotmail.com>
2018-09-09 21:07:10 -0500

    MV-030 Home bugs (#27)
    * MV-030 Home bugs
    
    https://trello.com/c/IIKlhtqI/30-home
    
    * Eliminar logo

--- src/modules/Categories/CategoryList.tsx
+++ src/modules/Categories/CategoryList.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { Icon } from "native-base";
 import * as rne from 'react-native-elements';
 import { ScrollView, Text, Image, View, Dimensions, TouchableHighlight } from "react-native";
 import styled from "styled-components"
 import { NavigationScreenProp } from 'react-navigation';
 import * as _ from 'lodash';
 import { connect } from 'react-redux';
+import Layout from "../shared/Layout";
 import { IStore } from '../../store/reducers/index';
 import { CategoriesActions } from '../../store/actions/CategoriesActions';
 import { Routes } from '../../Routes';
@@ -25,11 +25,6 @@ type State = {
 const screen_width = Dimensions.get("window").width;
 
 const S = {
-  Layout: styled(View) `
-      flex: 1;
-      padding-top: 20px;
-      background-color: #EAE9EF;
-  `,
   Header: styled(View) `
       flex-direction: row;
       justify-content: space-between;
@@ -50,12 +45,6 @@ const S = {
     height: 100%;
     border-radius: 25px;
   `,
-  View: styled(View) `
-    align-items: center;
-    flex-direction: row;
-    background-color: #aa072a;
-    flex:1;
-  `,
   Item: styled(View) `
     width: ${(screen_width / 4) - 10}px;
     height: 75px;
@@ -73,13 +62,11 @@ class CategoryList extends React.Component<Props, State> {
     this.props.actions.resetCheckout();
     this.props.actions.getCategories();
   }
-
-  someMethod() {
-
-  }
-
   searchCategory = (filter:string) => {
     this.props.actions.getCategories(filter);
+  }
+  clearCategory = (): void => {
+    this.searchCategory('');
   }
   openMenu = () => {
     this.props.navigation.openDrawer();
@@ -88,27 +75,24 @@ class CategoryList extends React.Component<Props, State> {
     this.props.navigation.navigate(Routes.RestaurantList);
   }
 
+  goToCheckout = () => {
+    this.props.navigation.navigate(Routes.Carrito);
+  };
+
   render() {
     return (
-      <S.Layout>
-        <S.Header>
-          <Icon name='menu' onPress={this.openMenu} />
-          <Text style={{ fontSize: 30 }}>Categorías</Text>
-          <Icon name='cart' active />
-        </S.Header>
+      <Layout
+          iconLeft="menu"
+          iconRight="cart"
+          onPressLeft={this.openMenu}
+          onPressRight={this.goToCheckout} >
         <ScrollView>
-          <S.View style={{ width: screen_width, height: 200 }}>
-              <S.Image
-                  style={{height: 150}}
-                  source={require('../../../assets/images/hungrylogo.png')}
-              />
-          </S.View>
           <rne.SearchBar
             clearIcon={{color: 'black'}}
             lightTheme
             round
             onChangeText={this.searchCategory}
-            onClear={this.searchCategory}
+            onClearText={this.clearCategory}
             icon={{ type: 'font-awesome', name: 'search' }}
             placeholder='¿Qué te provoca comer hoy?' />
 
@@ -125,7 +109,7 @@ class CategoryList extends React.Component<Props, State> {
             }
           </S.Options>
         </ScrollView>
-      </S.Layout>
+      </Layout>
     );
   }
 }
